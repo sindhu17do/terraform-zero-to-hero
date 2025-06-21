0661f531adcd7ede030e7ea1845524fc3f3427c3
sindhu17do <sindhura.p1791@gmail.com>
2025-06-21 19:10:41 +0000

    added subnet and key

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-020cba7c55df1f615"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id = "subnet-01285cd8621cf2828"
+    key_name = "new-key"
+    
 }
